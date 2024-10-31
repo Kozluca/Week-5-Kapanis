37116734a9448bfc51ebcdf792c6c3835204a23f
Kozluca <okozluca.1@gmail.com>
2024-10-31 14:58:53 +0300

    Proje dosyası ekle.

---
+++ Week-5-Kapanis/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_Kapanis
+{
+    public class Car
+    {
+        DateTime ProductionDate;
+        public string SerialNumber;
+        public string Brand;
+        public string Model;
+        public string Color;
+        public int DoorNumber;
+
+        public Car(DateTime productionDate, string serialNumber, string brand, string model, string color, int doorNumber)
+        {
+            ProductionDate = productionDate;
+            SerialNumber = serialNumber;
+            Brand = brand;
+            Model = model;
+            Color = color;
+            DoorNumber = doorNumber;
+        }
+        public static void CarList(List<Car> Carliste)
+        {
+
+            foreach (var Car in Carliste)
+            {
+                Console.WriteLine($"Arabanın Markası : {Car.Brand}");
+                Console.WriteLine($"Arabanın Modeli : {Car.Model}");
+                Console.WriteLine($"Arabanın Rengi: {Car.Color}");
+                Console.WriteLine($"Arabanın Kapısayısı : {Car.DoorNumber}");
+                Console.WriteLine($"Arabanın SeriNumarası: {Car.SerialNumber}");
+                Console.WriteLine($"Arabanın Üretim Tarihi : {DateTime.Now}");
+                Console.WriteLine("*****************************");
+
+            }
+        }
+    }
+}
---
+++ Week-5-Kapanis/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_Kapanis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> Carliste = new List<Car>();                         // Car sınıfından Lise oluşturduk
+        Başla:
+            Console.WriteLine("ARABA ÜRETMEK İSTER MİSİNİZ ?");
+            string answer = Console.ReadLine();                                 //Kullanıcıdan cevap aldık
+            answer = answer.ToLower();                                          //Kücük Harfe çevirdik
+        Uret:
+            if (answer == "h")                                                  // cevap h ise program kapatılacak
+            {
+                Console.WriteLine("Çıkış Yapılıyor...");
+
+            }
+            else if (answer == "e")                                             // cevap e ise araba bilgilerini kullanıcadan aldık
+            {
+                Console.WriteLine("Arabanın Markasını Girin.");
+                string input2 = Console.ReadLine();
+                Console.WriteLine("Arabanın Modelini Girin.");
+                string input3 = Console.ReadLine();
+                Console.WriteLine("Arabanın Rengini Girin.");
+                string input4 = Console.ReadLine();
+                Console.WriteLine("Arabanın Seri Numarasını Girin.");
+                string input = Console.ReadLine();
+            ORTA:
+                try                   // Kapı sayısının yanlıs tipte girilmesi durumunda geriye hata verecek
+                {
+                    Console.WriteLine("Arabanın Kapı Sayısını Girin.");
+                    int input5 = Convert.ToInt32(Console.ReadLine());
+                    Carliste.Add(new Car(DateTime.Now, input, input2, input3, input4, input5));
+                }
+                catch               // hatalı giriş olursa catch çalısır , ve goto Komutunda ki yere gönderir programı
+                {
+                    Console.WriteLine("Hatalı veri girdiniz");
+                    goto ORTA;
+                }
+
+                Console.WriteLine("------------------------");
+               son:
+                Console.WriteLine("Başka araba üretmek ister misiniz ?");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "evet")
+                {
+                    goto Uret;
+                }
+                else if (answer2 == "hayır")
+                {
+                    Car.CarList(Carliste);
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı cevap");
+                    goto son;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz Cevap");
+                goto Başla;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
